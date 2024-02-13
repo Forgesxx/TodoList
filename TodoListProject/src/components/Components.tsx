ee95b1cd19c5f2f2a9f68ef5803c0ea9cdba1d5f
ROG\forge <Forgesx236@gmail.com>
2024-02-13 22:16:52 +0200

    Made the first functionality. Added input line and button. After entering the text and pressing the button, the entered text is displayed on the screen.

---
+++ TodoListProject/src/components/Components.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Text, View } from "react-native"
+
+export const ComponentMain: React.FC = () =>{
+    return(
+        <View>
+        <Text>TaskName</Text>
+        
+        
+        </View>
+
+    )
+}
--- TodoListProject/src/screens/MainScreen.tsx
+++ TodoListProject/src/screens/MainScreen.tsx
@@ -1,11 +1,70 @@
-import React from "react";
-import { StyleSheet, FlatList, View, Text} from "react-native";
-export const MainScreen = ({route}: any) => {
-   
+import React, { useState } from "react";
+import { StyleSheet, TextInput, View, Text, TouchableOpacity} from "react-native";
+import { FlatList } from "react-native-gesture-handler";
+
+
+
+
+
+const MainScreen = () => {
+    const [inputText, setInputText] = useState(''); 
+    const [lines, setLines] = useState<string[]>([]);
+
+    const hadleInputText = (inputText: string) =>{
+        setInputText(inputText)
+    } 
+  
+    const handleButtonPress = () => {
+        setLines([...lines, inputText]);
+        setInputText('');
+      };
+    
+
+  
+    
+    
+    
     return(
 <View>
 
+<FlatList
+        data={lines}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => <Text>{item}</Text>}
+      />
+ 
+ 
+    <TextInput style={styles.textInput} value={inputText} onChangeText={hadleInputText} >
+
+    </TextInput>
+    
+    
+    <TouchableOpacity style={styles.addButton} onPress={handleButtonPress} >
+        <Text> 
+
+        </Text>
+    </TouchableOpacity>
 </View>  
   )
 }
 export default MainScreen
+
+
+const styles = StyleSheet.create({
+    mainContainer:{
+
+    },
+    textInput:{
+        color: "white",
+        backgroundColor: 'black',
+        borderRadius: 15
+
+    },
+    addButton:{
+        backgroundColor: 'blue',
+        padding: 5,
+        borderRadius:100,
+        width: 30,
+    },
+   
+})

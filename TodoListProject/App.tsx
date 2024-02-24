6d71fe10d3f4bf84927015590ef05eb6872104ad
ROG\forge <Forgesx236@gmail.com>
2024-02-24 20:33:48 +0200

    no message

--- TodoListProject/App.tsx
+++ TodoListProject/App.tsx
@@ -1,23 +1,18 @@
-import * as React from 'react'
-import  { NavigationContainer} from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import MainScreen from './src/screens/MainScreen';
 import SecondScreen from './src/screens/SecondScreen';
 
-
-
-const Stack = createStackNavigator()
- function App(): React.JSX.Element {
- 
+const Stack = createStackNavigator();
+function App(): React.JSX.Element {
   return (
- <NavigationContainer>
-  <Stack.Navigator>
-    <Stack.Screen name="MainScreen" component={MainScreen}></Stack.Screen>
-    <Stack.Screen name="SecondScreen" component={SecondScreen}></Stack.Screen>
-  </Stack.Navigator>
-
- 
- </NavigationContainer>
-  )
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="MainScreen" component={MainScreen} />
+        <Stack.Screen name="SecondScreen" component={SecondScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
-export default App
+export default App;

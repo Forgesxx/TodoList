37df8de6715fc9a0f4a2e0db73175cbdf8ef87f4
Danil Korotenko <danil.korotenko@gmail.com>
2024-02-25 13:45:41 +0200

    Fixed styling in eslint config.

--- TodoListProject/.eslintrc.js
+++ TodoListProject/.eslintrc.js
@@ -1,45 +1,21 @@
-module.exports = 
-{
-    root: true,
-    extends: '@react-native',
-    rules:
-    {
-        semi: ['error', 'always',],
-        "object-shorthand": ["off",],
-        'brace-style': ['error', 'allman',],
-        indent:
-        [
-            'error', 4,
-            {
-                outerIIFEBody: 1,
-                FunctionExpression: { body: 1, parameters: 2, },
-                SwitchCase: 1,
-            },
-        ],
-        'no-unused-vars': ['off', { vars: 'local', },],
-        'no-multi-spaces': ['off',],
-        quotes: ["off",],
-        "space-before-function-paren": ["off",],
-        "comma-dangle":
-        [
-            "error",
-            {
-                arrays: "always",
-                objects: "always",
-                imports: "always",
-                exports: "always",
-                functions: "never",
-            },
-        ],
-        "keyword-spacing":
-        [
-            "error",
-            {
-                overrides:
-                {
-                    catch: { after: false, },
-                },
-            },
-        ],
-    },
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    semi: ['error', 'always'],
+    'object-shorthand': ['off'],
+    'brace-style': ['error', 'allman'],
+    'no-unused-vars': ['off', {vars: 'local'}],
+    'no-multi-spaces': ['off'],
+    quotes: ['off'],
+    'space-before-function-paren': ['off'],
+    'keyword-spacing': [
+      'error',
+      {
+        overrides: {
+          catch: {after: false},
+        },
+      },
+    ],
+  },
 };

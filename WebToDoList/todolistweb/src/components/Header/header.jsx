5698f845872bd4186717b183743a92065f6b3f24
ROG\forge <Forgesx236@gmail.com>
2024-02-28 10:34:30 +0200

    Started working with the web version of the application

---
+++ WebToDoList/todolistweb/src/components/Header/header.jsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from 'react'
+
+
+
+
+  export default function Header() {
+    const [currentTime, setNow] = useState(new Date());
+  
+    useEffect(() => {
+      const intervalId = setInterval(() => setNow(new Date()), 1000);
+      return () => {
+        clearInterval(intervalId);
+      };
+    }, []); // Make sure to include an empty dependency array to run the effect only once on mount
+  
+    return (
+      <div>
+        <span>Time now: {currentTime.toLocaleTimeString()}</span>
+      </div>
+    );
+  }
---
+++ WebToDoList/todolistweb/src/components/InputContainer/InputContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './inputContainer.css';
+
+export default function InputContainer() {
+  return (
+    <div className='input-container'>
+      <input className='text-input' placeholder='Enter text'></input>
+      <button className='action-button'>Click</button>
+    </div>
+  );
+}

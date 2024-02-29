cff427a37f49d51988df6c3e05efbc91d1dabad1
Danil Korotenko <danil.korotenko@gmail.com>
2024-02-29 09:55:05 +0200

    Just a little cosmetic changes.

--- TodoListProject/__tests__/App.test.tsx
+++ TodoListProject/__tests__/App.test.tsx
@@ -7,13 +7,13 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import {it, } from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', 
-    () => 
+it('renders correctly',
+    () =>
     {
         renderer.create(<App />);
     });
--- TodoListProject/jest.config.js
+++ TodoListProject/jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  preset: 'react-native',
+    preset: "react-native",
+    transformIgnorePatterns: [
+        "node_modules/(?!(jest-)?react-native)",
+        "node_modules/?!(react-navigation)",
+        'node_modules/(?!@react-native|react-native)',
+    ],
 };

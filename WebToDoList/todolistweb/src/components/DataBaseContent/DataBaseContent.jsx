87a07548dd99a4ea6aa8ec4952f0499e51e475ae
ROG\forge <Forgesx236@gmail.com>
2024-03-04 15:35:31 +0200

    temp

--- WebToDoList/todolistweb/src/components/DataBaseContent/DataBaseContent.jsx
+++ WebToDoList/todolistweb/src/components/DataBaseContent/DataBaseContent.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, } from 'react';
 import ApiService from './apiServise';
+import './DataBaseContent.css';
 
 function DataBaseContent()
 {
@@ -17,14 +18,19 @@ function DataBaseContent()
     }, []);
 
     return (
-        <div>
-            <div>
+        <div className="data-container">
+            <div className="items-container">
                 {items.map((item) => (
-                    <div key={item.id}>{item.item}</div>
+                    <div key={item.id} className="item">
+                        {item.item}
+                        <div className="button-container">
+                            <button >1</button>
+                            <button >2</button>
+                        </div>
+                    </div>
                 ))}
             </div>
         </div>
     );
 }
-
 export default DataBaseContent;
